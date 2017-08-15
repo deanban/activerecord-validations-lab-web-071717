e88105aaf69bb355811e2cd572e276a28ac4d04b
deanban <pbp2005@gmail.com>
2017-08-15 13:22:43 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: ["Fiction", "Non-Fiction"], messege: "Invalid Category"}
+	validate :clickbait
+
+	def clickbait
+		if title != nil && !title.start_with?('Won\'t Believe', 'Top', 'Secret', 'Guess', 'You Won\'t')
+			errors.add(:title, "Not clickbait title")
+		end
+	end
 end
